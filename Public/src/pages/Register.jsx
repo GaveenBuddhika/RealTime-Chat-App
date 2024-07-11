63f41f33039f7b96a737ed5311376caa0c889e90
Gaveen_Buddhika <buddhik-se20022@stu.kln.ac.lk>
2024-07-11 15:26:11 +0530

    create pahes and  register form

---
+++ Public/src/pages/Register.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import Logo from '../assets/logo.svg';
+import { Link } from "react-router-dom";
+
+export const Register = () => {
+
+ const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('form');
+
+};
+
+const handleChange = (event) => {};
+
+
+
+
+  return (
+    <>
+      
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h1>CHATTY</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login.</Link>
+          </span>
+        </form>
+      
+      
+    </>
+  );
+}
+
+export default Register;
--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,30 @@
-const express = reaquire("express");
-const cors = reaquire("cors");
-const mongoose = reaquire("mongoose");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 
 const app = express();
-const socket = require("socket.io");
-require("dotenv").config();
+require('dotenv').config();
+const port = 4000;
+
+app.use(cors());
+app.use(express.json());
+const mongoUri = "mongodb+srv://gaveen:pass123@cluster0.3mwbfgj.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  });
+
+
+
+const server = app.listen(port,()=>{
+
+console.log(`server running in ${port}`)
+
+
+});
